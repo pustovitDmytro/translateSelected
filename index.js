80210168a36869bbfcfd9fbb0823d18b3db18a7f
pustovitDmytro <pustic@gmail.com>
2017-01-11 21:23:28 +0200

    added activated property, added changing toolbar button ico

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ var buttons = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
 var data = require("sdk/self").data;
 var selection = require("sdk/selection");
+var activated = require('sdk/simple-prefs').prefs['activated']
+
 var button = buttons.ActionButton({
   id: "github-link",
   label: "Visit Github",
-  icon: {
-    "16": "./test_ico.png"
+  icon:  {
+    "16": "./icon-16.png"  
   },
   onClick: handleClick
 });
-var myPanel = require("sdk/panel").Panel({
-  //width: 180,
-  height: 180,
+
+var Popup = require("sdk/panel").Panel({
+  height: 200,
   contentURL: "./popup.html",
   contentScriptFile: "./popup.js"
 });
@@ -20,11 +22,16 @@ var myPanel = require("sdk/panel").Panel({
 tabs.open("https://en.wikipedia.org/wiki/Liverpool_F.C.");
 
 selection.on('select', function () {
-      myPanel.port.emit("selection",selection.text)
+       Popup.port.emit("selection",selection.text);
 	console.log('Selected : ['+selection.text+'];');
-  	myPanel.show();
+  	Popup.show();
 });
 
 function handleClick(state) {
-  tabs.open("https://github.com/pustovitDmytro/translateSelected");
+  activated=!activated;
+  if(activated) button.icon = {
+    "16": "./icon-16.png"  
+  };else button.icon = {
+    "16": "./test_ico.png"  
+  }
 }
